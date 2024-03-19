6c60c9208d486c33edde0ce11a785f6c481ad5f4
vincent <vinnymummo@gmail.com>
2024-03-19 12:36:10 +0300

    math commit

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let multiply;
+let num1 = 2
+let num2 = 31
+multiply = num1 * num2;
+
+
+let random;
+random = Math.ceil(Math.random() + 1);
+
+let mod;
+let num3 = 16
+let num4 = 6
+mod = num3 % num4;
+
+let max;
+max = Math.max(2,4,6,15,4,20);
 
 // Write your code here
